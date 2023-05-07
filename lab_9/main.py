e610954eaaad7fe6f2f92c6c86eb3ba81b5d54b0
Андрей <andrej@MacBook-Pro-Andrej.local>
2023-05-07 22:09:25 +0300

    main

--- lab_9/main.py
+++ lab_9/main.py
@@ -9,38 +9,39 @@ def task1():
     Path('new_images').mkdir(parents=True, exist_ok=True)
 
     for file in filenames:
-        if file.suffix in ['.jpg', '.png']:
+        if file.suffix in ['.jpg', '.png']:git
             with Image.open(file) as img:
                 img.load()
                 new_img = img.filter()
                 new_img.save(Path("new_images", file))
 
 
-# def task3():
-#
-#     # Имеется файл с данными в формате csv:
-#     # Продукт,Количество,Цена
-#     # Молоко,2,80
-#     # Сыр,1,500
-#     # Хлеб,2,70
-#     # Напишите программу, которая считывает данные из этого файла,
-#     # подсчитывает итоговую сумму расходов и выводит данные в виде:
-#     # Нужно купить:
-#     # Молоко - 2 шт. за 80 руб.
-#     # Сыр - 1 шт. за 500 руб.
-#     # Хлеб - 2 шт. за 70 руб.
-#     # Итоговая сумма: 800 руб.
-#
-#
-#     import csv
-#     from pathlib import Path
-#
-#     file = open("data.csv," "r")
-#     data = list(csv.reader(file, delimiter=", "))
-#     print("Нужно купить: ")
-#     for i in data:
-#         print(f"{i[0]} - {i[1]} шт. за {i[2]} руб.")
-#     print(f"Итоговая сумма: {sum([int(i[1])*int(i[2]) for i in data])} руб.")
-#     file.close()
+def task3():
+
+    # Имеется файл с данными в формате csv:
+    # Продукт,Количество,Цена
+    # Молоко,2,80
+    # Сыр,1,500
+    # Хлеб,2,70
+    # Напишите программу, которая считывает данные из этого файла,
+    # подсчитывает итоговую сумму расходов и выводит данные в виде:
+    # Нужно купить:
+    # Молоко - 2 шт. за 80 руб.
+    # Сыр - 1 шт. за 500 руб.
+    # Хлеб - 2 шт. за 70 руб.
+    # Итоговая сумма: 800 руб.
+
+
+    import csv
+    from pathlib import Path
+
+    file = open("data.csv," "r")
+    data = list(csv.reader(file, delimiter=", "))
+    print("Нужно купить: ")
+    for i in data:
+        print(f"{i[0]} - {i[1]} шт. за {i[2]} руб.")
+    print(f"Итоговая сумма: {sum([int(i[1])*int(i[2]) for i in data])} руб.")
+    file.close()
 
 task1()
+task3()
